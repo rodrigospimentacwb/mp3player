9caa9365d830f16b970c6587ebb98ad26206c556
rodrigospimentacwb <rodrigospimenta@gmail.com>
2023-08-29 16:41:37 -0300

    first commit

---
+++ src/main/java/com/grupo3/mp3player/Mp3playerApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.mp3player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3playerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3playerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/mp3player/Mp3playerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.mp3player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp3playerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
